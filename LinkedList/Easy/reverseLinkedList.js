28ef9967c2441ea479d421b9086a72dafaee72c2
Le <phamtle724@gmail.com>
2022-11-03 02:11:00 -0700

    moved file, add comment to rev linkedlist

--- LinkedList/Easy/reverseLinkedList.js
+++ LinkedList/Easy/reverseLinkedList.js
@@ -8,17 +8,23 @@
 /**
  * @param {ListNode} head
  * @return {ListNode}
+ *
+ * 1 -> 4 -> 2 -> 7 -> 9 -> 5
+ *
+ * 1 <- 4 -< 2 <- 7 <- 9 <- 5
  */
  var reverseList = function(head) {
   if (!head || !head.next) return head;
   let next = null;
-  let curr = head;
+  let curr = head; //curr = 1
   let prev = null;
   while (curr !== null) {
-      next = curr.next; //save next to use next iteration
-      curr.next = prev; //rotate the arrow to point to its left
-      prev = curr; //moving on, now curr is just in the past
-      curr = next; //now curr is ready for next iteration
+      next = curr.next; //save next to use next iteration //next = 4 / 2
+      curr.next = prev; //rotate the arrow to point to its left //curr.next = null /
+      prev = curr; //moving on, now curr is just in the past //prev = 1
+      curr = next; //now curr is ready for next iteration //curr = 4
+
+      // ... -> 4 -> 1 -> null
   }
       head = prev; //now new head is last curr that's not null
   return head;
