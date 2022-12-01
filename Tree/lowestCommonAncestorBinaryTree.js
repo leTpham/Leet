92f7f955007a40505569bf59596bf6546c1afed2
Le Pham <phamtle724@gmail.com>
2022-11-30 21:52:02 -0800

    add psuedo and docstring

--- Tree/lowestCommonAncestorBinaryTree.js
+++ Tree/lowestCommonAncestorBinaryTree.js
@@ -1,18 +1,26 @@
 "use strict";
 
+/** Given a binary search tree (BST), find the lowest common ancestor (LCA)
+ * node of two given nodes in the BST.
+ * According to the definition of LCA on Wikipedia:
+ * “The lowest common ancestor is defined between two nodes p and q as the
+ * lowest node in T that has both p and q as descendants (where we allow a node
+ * to be a descendant of itself).”
+ */
+
+//Pseudocode: basically go until finding p and q split into left-right
 function lowestCommonAncestor(root, p, q) {
   let curr = root;
 
   while (curr) {
-    if (p.val > curr.val && q.val > curr.val ) {
+    if (p.val > curr.val && q.val > curr.val) {
       curr = curr.right;
     }
     else if (p.val < curr.val && q.val < curr.val) {
       curr = curr.left;
     }
     else {
-      break
+      return curr;
     }
   }
-  return curr;
 }
