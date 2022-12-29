031f0643b2e718335088cdf0656eb05cc8491954
Le Pham <phamtle724@gmail.com>
2022-12-28 22:23:45 -0800

    add comments threeSum

--- Array/Easy/threeSum.js
+++ Array/Easy/threeSum.js
@@ -7,25 +7,44 @@
 
 function threeSum(nums) {
   let res = [];
-  nums.sort((a,b) => a - b);
+  nums.sort((a, b) => a - b);
   if (nums.length < 3) return res;
-  for (let i = 0; i < nums.length -2; i++) {
+
+  //i as left-most anchor
+  for (let i = 0; i < nums.length - 2; i++) {
+    // once i's value is 0, stop since positive numbers can't add up to 0
     if (nums[i] > 0) return res;
-    if (nums[i] === nums[i-1]) continue;
+
+    //since we don't want repeats, skip if we've already seen
+    if (nums[i] === nums[i - 1]) continue;
+
     let j = i + 1;
     let k = nums.length - 1;
+
+    //for each i of nums array, have a while loop to check for
+    //all possible combos for that i
     while (j < k) {
       let sum = nums[i] + nums[j] + nums[k];
+
+      //if find the combo, keep increment j and decrement k for other possible ones
+      //with the same anchor i (we can do this because we have sorted the array)
       if (sum === 0) {
         res.push([nums[i], nums[j], nums[k]]);
-        while (nums[j] === nums[j+1]) j++;
-        while (nums[k] === nums[k-1]) k--;
+
+        //skip j and k if duplicate within the same anchor i
+        while (nums[j] === nums[j + 1]) j++;
+        while (nums[k] === nums[k - 1]) k--;
+
+        //move j and k toward each other to check for other combos for current i
         j++;
         k--;
       }
+      //if sum is too small, we need it to be larger => needs to inch j to the right
       else if (sum < 0) {
         j++;
       }
+      //if sum is too big, we need it to be smaller => needs to inch k to the left
+
       else {
         k--;
       }
